73f1c241cf097f227517c28c3d72e478d8161de2
lam <i.mailed.lam@gmail.com>
2023-11-13 15:31:57 +0200

    MAnipulating DOM

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var firstListItem = document.querySelector("ul li");
+//console.log(firstListItem);
+
+firstListItem.style.textTransform = "uppercase";
+
+var moreReasons = document.querySelector(".more-reasons");
+//console.log(moreReasons);
+
+moreReasons.style.fontSize = "2.5em";
+
+var whyJS = document.querySelector("h3");
+//console.log(whyJS);
+
+whyJS.innerHTML = 'why learn <span class="highlight"> JavaScript </span>??';
+//remember single and double Qs
+
+var highlight = document.querySelector(".highlight");
+
+highlight.style.backgroundColor = "#ffff82";
+
+var mainImage = document.querySelector("img");
+//console.log(mainImage);
+
+mainImage.src = "img/js-code.png";
+mainImage.alt = "JavaScritp code example";
+// GREAT .. worked !!
+
